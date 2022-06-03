a452e7a02825fe9ac0b893e1f9c3736dabe2ccd9
Sam <sm2@soas.ac.uk>
2022-06-03 12:44:01 +0100

    last commit before woking on moving backgrounds

--- script.js
+++ script.js
@@ -37,38 +37,15 @@ class Player{
 let player = new Player;
 player.draw();
 
+let background = new Image();
+background.src = 'img/hercule_seghers1.jpg';
+
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(background, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   player.draw();
   player.update();
   requestAnimationFrame(animate);
 }
 animate();
 
-//class Enemy{
-//  constructor(){
-//    this.image = new Image();
-//    this.image.src = 'img/ladybird_sprite_4_frame.png';
-//    this.x = Math.random() * canvas.width;
-//    this.y = Math.random() * canvas.height;
-//    this.speed = Math.random() * 4 - 2; // -2 < x < 2
-//    this.spriteWidth = 241;
-//    this.spriteHeight = 127;
-//    this.width = this.spriteWidth / 1.5;
-//    this.height = this.spriteHeight / 1.5;
-//    this.frame = 0; // for cycling through sprite sheet
-//    this.flapSpeed = Math.floor(Math.random() * 5 + 3); //;make wings not sync
-//  }
-//  update(){
-//    this.x += this.speed;
-//    this.y += this.speed;
-//    // animate sprites
-//    if (gameFrame % this.flapSpeed === 0){
-//      this.frame > 2 ? this.frame = 0: this.frame++; // ternary operator!
-//    }
-//
-//  }
-//  draw(){
-//    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
-//  }
-//};
